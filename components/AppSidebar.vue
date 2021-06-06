020a6f7222f2b13c4d847cbc9996b503109ad046
Chris Schweda <cschweda@gmail.com>
2021-06-06 13:25:02 -0500

    fix: sidebar style skeleton v-list

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -6,18 +6,35 @@
     disable-resize-watcher
     color="grey lighten-4"
   >
-    <div style="font-size: 16px; font-weight: 900; margin-bottom: 20px">
-      Sidebar TOC here
+    <div
+      style="font-size: 16px; font-weight: 900; margin-bottom: 20px"
+      class="text-center mt-3"
+    >
+      Sidebar Table of Contents here
     </div>
     <div
       v-if="els && els.length"
       style="padding-left: 15px; padding-right: 5px"
     >
-      <div v-for="(item, index) in els" :key="index">
-        <span class="hover" @click="gotoHeading(item)">{{
-          item.innerHTML
-        }}</span>
-      </div>
+      <!-- <li
+        v-for="(item, index) in els"
+        :key="index"
+        class="hover"
+        @click="gotoHeading(item)"
+      >
+        {{ item.innerHTML }}
+      </li> -->
+      <v-list dense style="margin-top: -10px">
+        <v-list-item
+          v-for="(item, index) in els"
+          :key="index"
+          class="hover"
+          style="font-size: 12px; font-weight: 700"
+          @click="gotoHeading(item)"
+        >
+          <v-list-item-content> {{ item.innerHTML }}</v-list-item-content>
+        </v-list-item>
+      </v-list>
     </div>
   </v-navigation-drawer>
 </template>
